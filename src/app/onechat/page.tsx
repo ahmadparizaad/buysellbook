fd7a6c9fcb397932e4ccb894cfaf2ed60f6e8d3c
Mohammad Ahmad <104555382+ahmadparizaad@users.noreply.github.com>
2025-01-04 13:20:04 +0530

    Remove ConversationsList component and inline its rendering logic in OneChat
    - Deleted the ConversationsList component to simplify the code structure.
    - Integrated the rendering logic directly into the OneChat component, maintaining the same functionality for displaying recent chats.
    - This change enhances code clarity by reducing component complexity and improving maintainability.

--- src/app/onechat/page.tsx
+++ src/app/onechat/page.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { useSearchParams } from 'next/navigation';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
-import ConversationsList from '../ConversationsList/page';
 import { cometchatAuth } from '@/utils/cometchatAuth';
 import Chat from '../chat/page';
 
@@ -196,7 +195,27 @@ const OneChat = () => {
     return (
         <div className="h-[100vh] flex flex-col border rounded-md">
             {!selectedChat ? (
-            <ConversationsList conversations={conversations} startChat={startChat} />
+            <div className="flex-1 overflow-y-auto p-4">
+            
+            <h2 className="text-xl font-semibold mb-4 mx-3 mt-20 md:mt-24">Recent Chats</h2>
+            {conversations.map((conversation) => (
+                <div
+                    key={conversation.uid}
+                    className="border-b rounded-xl p-5 hover:bg-gray-800 cursor-pointer"
+                    onClick={() => startChat(conversation.uid)}
+                >
+                    <div className="font-medium">{conversation.name || conversation.uid}</div>
+                    <div className="flex justify-between text-sm pt-2 text-gray-400">
+                        {conversation.lastMessage || 'No messages yet'}
+                        {conversation.timestamp && (
+                            <div className="ml-auto right-2 text-xs text-gray-400">
+                                {new Date(conversation.timestamp * 1000).toLocaleString()}
+                            </div>
+                        )}
+                    </div>
+                </div>
+            ))}
+</div>
         ) : (
             <Chat senderUID={senderUID} recieverUID={recieverUID} setSelectedChat={setSelectedChat}/>
         )}
