a5056081e6a1bbfc82d149f41aa6d4bc66407b11
Mohammad Ahmad <104555382+ahmadparizaad@users.noreply.github.com>
2025-01-25 13:40:55 +0530

    Add PostCSS plugins for Tailwind CSS and update layout with ad scripts
    - Configured PostCSS config to include `postcss-import` and `tailwindcss/nesting` plugins
    - Added production-specific ad scripts to the root layout component
    - Improved CSS processing and added monetization scripts for production environment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/Footer";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -45,7 +44,32 @@ export default function RootLayout({
         <Footer/>
         </LoadingProvider>
         </Suspense>
-      </body>
+        {process.env.NODE_ENV === "production" && (
+          <>
+        <script async type='text/javascript' src='//pl25672536.profitablecpmrate.com/92/b6/49/92b6496befa6e7512faa3efd72b8ac84.js'></script>
+        <script type="text/javascript">
+        atOptions = {`{
+          'key' : '0f829db4326cd0b19b77a0058cc26c4d',
+          'format' : 'iframe',
+          'height' : 300,
+          'width' : 160,
+          'params' : {}
+        }`};
+      </script>
+      <script async type="text/javascript" src="//www.highperformanceformat.com/0f829db4326cd0b19b77a0058cc26c4d/invoke.js"></script>
+      <script type="text/javascript">
+      atOptions = {`{
+        'key' : '0589a46fadc2635261f5d7864e25bf69',
+        'format' : 'iframe',
+        'height' : 50,
+        'width' : 320,
+        'params' : {}
+      }`};
+    </script>
+    <script async type="text/javascript" src="//www.highperformanceformat.com/0589a46fadc2635261f5d7864e25bf69/invoke.js"></script>
+        </>
+        )}
+        </body>
     </html>
   );
 }
